4268bf9c09e3ec61ba386f6679515cc39a8f269d
Marco Gallegos <marcogallegos@ciencias.unam.mx>
2022-11-15 17:50:23 -0500

    solutions tutorial day 2 included

--- FirstAnnualMeeting/Day2/aux-fcts.R
+++ FirstAnnualMeeting/Day2/aux-fcts.R
@@ -1,4 +1,4 @@
-forward_algorithm = function(observations,time,init = pi,tpm=Gamma.matrix,parameters=lambda){
+forward_algorithm = function(observations,time,init = pi_,tpm=Gamma.matrix,parameters=lambda){
   
   # This is for time t = 1
   alpha = init %*% diag(dpois(observations[1],parameters))
